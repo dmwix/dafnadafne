78219c700e074e33340a86c7b0b598dbe02a20df
dmwix <87998561+dmwix@users.noreply.github.com>
2022-07-18 17:05:58 -0300

    galería

--- js/main.js
+++ js/main.js
@@ -38,42 +38,76 @@ links.forEach(link => {
 const draggables = document.querySelectorAll('.draggable');
 
 draggables.forEach(draggable => {
-    draggable.onmousedown = function(event) {
-
-        let shiftX = event.clientX - draggable.getBoundingClientRect().left;
-        let shiftY = event.clientY - draggable.getBoundingClientRect().top;
-      
-        draggable.style.position = 'absolute';
-        draggable.style.zIndex = 1000;
-        document.body.append(draggable);
-      
+    draggable.onmousedown = function(event) { 
+      let shiftX = event.clientX - draggable.getBoundingClientRect().left;
+      let shiftY = event.clientY - draggable.getBoundingClientRect().top;
+    
+      draggable.style.position = 'absolute';
+      draggable.style.zIndex = 1000;
+      document.body.append(draggable);
+    
+      moveAt(event.pageX, event.pageY);
+    
+      function moveAt(pageX, pageY) {
+        draggable.style.left = pageX - shiftX + 'px';
+        draggable.style.top = pageY - shiftY + 'px';
+      }
+    
+      function onMouseMove(event) {
         moveAt(event.pageX, event.pageY);
-      
-        // moves the draggable at (pageX, pageY) coordinates
-        // taking initial shifts into account
-        function moveAt(pageX, pageY) {
-          draggable.style.left = pageX - shiftX + 'px';
-          draggable.style.top = pageY - shiftY + 'px';
-        }
-      
-        function onMouseMove(event) {
-          moveAt(event.pageX, event.pageY);
-        }
-      
-        // move the draggable on mousemove
-        document.addEventListener('mousemove', onMouseMove);
-      
-        // drop the draggable, remove unneeded handlers
-        draggable.onmouseup = function() {
-          document.removeEventListener('mousemove', onMouseMove);
-          draggable.onmouseup = null;
-        };
+      }
+    
+      document.addEventListener('mousemove', onMouseMove);
+    
+      draggable.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        draggable.onmouseup = null;
+      };
 
-        draggable.ondragstart = function() {
-            return false;
-          };
+      draggable.ondragstart = function() {
+          return false;
+      };      
       };
 
       draggable.addEventListener('mouseover', hideCursors);
       draggable.addEventListener('mouseout', showCursors);
 });
+
+// CATALOGO
+
+// filterSelection("all") // Execute the function and show all columns
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("column");
+//   if (c == "all") c = "";
+//   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+//   for (i = 0; i < x.length; i++) {
+//     w3RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//   }
+// }
+
+const todas = document.querySelectorAll('.todas')
+const playa = document.querySelectorAll('.playa')
+
+let pepito = document.querySelector('button');
+pepito.addEventListener(click, function(e) {
+  filterGallery(todas);
+  });
+
+function filterGallery(criterio) {
+  ocultarFotos(todas);
+  mostrarFotos(criterio);
+};
+
+function mostrarFotos(filtro) {
+  filtro.forEach(foto => {
+    foto.style.display = 'block';
+  });
+}
+
+function ocultarFotos(filtro) {
+  filtro.forEach(foto => {
+    foto.style.display = 'none';
+  });
+}
