cb4074f4ee123f9ad614fa522b4f5a8b3fe4f307
dmwix <87998561+dmwix@users.noreply.github.com>
2022-07-25 12:43:51 -0300

    apliqué prettier

--- js/main.js
+++ js/main.js
@@ -9,65 +9,63 @@
 let ancho = document.documentElement.clientWidth;
 let alto = document.documentElement.clientHeight;
 
-
 // CUSTOM CURSOR
 const cursor = document.getElementById("main-cursor");
 const followCursor = document.getElementById("follow-cursor");
 
-document.addEventListener('mousemove', function (e) {
+document.addEventListener("mousemove", function (e) {
   let cursorLeft = `${e.pageX - cursor.offsetWidth / 2}`;
-  if (cursorLeft > (ancho - cursor.offsetWidth)) {
-    cursorLeft = (ancho - cursor.offsetWidth)
-  };
+  if (cursorLeft > ancho - cursor.offsetWidth) {
+    cursorLeft = ancho - cursor.offsetWidth;
+  }
   let cursorTop = `${e.pageY - cursor.offsetHeight / 2}`;
-  if (cursorTop > (alto - cursor.offsetHeight)) {
-    cursorTop = (alto - cursor.offsetHeight)
-  };
-  
-  let followCursorLeft = `${e.pageX - 10}`;
-  if (followCursorLeft > (ancho - followCursor.offsetWidth)) {
-    followCursorLeft = (ancho - followCursor.offsetWidth)
-  };
-  let followCursorTop = `${e.pageY - 10}`;
-  if (followCursorTop > (alto - followCursor.offsetHeight)) {
-    followCursorTop = (alto - followCursor.offsetHeight)
+  if (cursorTop > alto - cursor.offsetHeight) {
+    cursorTop = alto - cursor.offsetHeight;
   }
 
-  cursor.style.left = cursorLeft + 'px',
-  cursor.style.top = cursorTop + 'px',
-  followCursor.style.left = followCursorLeft + 'px';
-  followCursor.style.top = followCursorTop + 'px';
+  let followCursorLeft = `${e.pageX - 10}`;
+  if (followCursorLeft > ancho - followCursor.offsetWidth) {
+    followCursorLeft = ancho - followCursor.offsetWidth;
+  }
+  let followCursorTop = `${e.pageY - 10}`;
+  if (followCursorTop > alto - followCursor.offsetHeight) {
+    followCursorTop = alto - followCursor.offsetHeight;
+  }
+
+  (cursor.style.left = cursorLeft + "px"),
+    (cursor.style.top = cursorTop + "px"),
+    (followCursor.style.left = followCursorLeft + "px");
+  followCursor.style.top = followCursorTop + "px";
 });
 
 function noCursors(array) {
-  array.forEach(el => {
-    el.addEventListener('mouseover', hideCursors);
-    el.addEventListener('mouseout', showCursors);
+  array.forEach((el) => {
+    el.addEventListener("mouseover", hideCursors);
+    el.addEventListener("mouseout", showCursors);
   });
-};
+}
 
 function hideCursors(e) {
-  cursor.style.opacity = '0';
-  followCursor.style.opacity = '0';
-};
+  cursor.style.opacity = "0";
+  followCursor.style.opacity = "0";
+}
 
 function showCursors() {
-  cursor.style.opacity = '1';
-  followCursor.style.opacity = '1';
-};
+  cursor.style.opacity = "1";
+  followCursor.style.opacity = "1";
+}
 
-const links = document.querySelectorAll('.link');
+const links = document.querySelectorAll(".link");
 noCursors(links);
-
-
+// mostrarle a dafi como queda sin esto
 
 // DRAGGABLE PHOTOS
-const draggables = document.querySelectorAll('.draggable');
+const draggables = document.querySelectorAll(".draggable");
 noCursors(draggables);
 
-draggables.forEach(draggable => {
+draggables.forEach((draggable) => {
   draggable.onmousedown = function (event) {
-    draggable.classList.remove('vibrar');
+    draggable.classList.remove("vibrar");
 
     let coords = draggable.getBoundingClientRect();
     let left = coords.left;
@@ -76,7 +74,7 @@ draggables.forEach(draggable => {
     let shiftX = event.clientX - left;
     let shiftY = event.clientY - top;
 
-    draggable.style.position = 'absolute';
+    draggable.style.position = "absolute";
     draggable.style.zIndex = 100;
     document.body.append(draggable);
     moveAt(event.pageX, event.pageY);
@@ -84,32 +82,32 @@ draggables.forEach(draggable => {
     function moveAt(pageX, pageY) {
       let draggableLeft = pageX - shiftX;
       if (draggableLeft < 0) draggableLeft = 0;
-      if (draggableLeft > (ancho - coords.width)) {
-        draggableLeft = (ancho - coords.width)
-      };
+      if (draggableLeft > ancho - coords.width) {
+        draggableLeft = ancho - coords.width;
+      }
       let draggableTop = pageY - shiftY;
       if (draggableTop < 0) draggableTop = 0;
       if (draggableTop > alto - coords.height) {
         draggableTop = alto - coords.height;
       }
 
-      draggable.style.left = draggableLeft + 'px';
-      draggable.style.top = draggableTop + 'px';
-    };
+      draggable.style.left = draggableLeft + "px";
+      draggable.style.top = draggableTop + "px";
+    }
 
     function onMouseMove(event) {
       moveAt(event.pageX, event.pageY);
-    };
+    }
 
-    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener("mousemove", onMouseMove);
 
     draggable.onmouseup = function () {
-      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener("mousemove", onMouseMove);
       draggable.onmouseup = null;
     };
 
     draggable.onmouseout = function () {
-      draggable.classList.add('vibrar');
+      draggable.classList.add("vibrar");
     };
 
     draggable.ondragstart = function () {
@@ -121,74 +119,74 @@ draggables.forEach(draggable => {
     };
 
     document.body.addEventListener("mouseout", function (event) {
-      if (event.clientY <= 0 || event.clientX <= 0 || (event.clientX >= window.innerWidth || event.clientY >= window.innerHeight)) {
-        document.removeEventListener('mousemove', onMouseMove);
+      if (
+        event.clientY <= 0 ||
+        event.clientX <= 0 ||
+        event.clientX >= window.innerWidth ||
+        event.clientY >= window.innerHeight
+      ) {
+        document.removeEventListener("mousemove", onMouseMove);
         draggable.onmouseup = null;
       }
     });
-
   };
 });
 
-
-
 // CATALOGO
-const todas = document.querySelectorAll('.todas');
-const filters = document.querySelectorAll('.filter');
+const todas = document.querySelectorAll(".todas");
+const filters = document.querySelectorAll(".filter");
 noCursors(filters);
 
 function filterGallery(criterio) {
   hidePhotos(todas);
   showPhotos(criterio);
-};
+}
 
 function hidePhotos(criterio) {
-  criterio.forEach(photo => {
-    photo.style.display = 'none';
+  criterio.forEach((photo) => {
+    photo.style.display = "none";
   });
-};
+}
 
 function showPhotos(criterio) {
-  criterio.forEach(photo => {
-    photo.style.display = 'block';
-    photo.classList.remove('reload-gallery');
+  criterio.forEach((photo) => {
+    photo.style.display = "block";
+    photo.classList.remove("reload-gallery");
     // reflow for triggering animation again
     photo.offsetHeight;
-    photo.classList.add('reload-gallery');
+    photo.classList.add("reload-gallery");
   });
-};
+}
 
-const filtersList = document.querySelector('.filters-list');
-filtersList.addEventListener('click', (e) => {
-  let filter = e.target.closest('.filter');
+const filtersList = document.querySelector(".filters-list");
+filtersList.addEventListener("click", (e) => {
+  let filter = e.target.closest(".filter");
   if (!filter) return;
   let filterName = filter.innerText;
-    // poner clase activa al filtro seleccionado en la lista
-  document.querySelector('.current').classList.remove('current');
-  filter.classList.add('current');
+  // poner clase activa al filtro seleccionado en la lista
+  document.querySelector(".current").classList.remove("current");
+  filter.classList.add("current");
   // aplicar filterGallery al seleccionado
   let selected = document.querySelectorAll(`.${filterName}`);
   filterGallery(selected);
   // actualizar url FUNCIÓN APARTE??
   let url = `${filterName}.html`;
   let pechuguita = `catálogo: ${filterName}`;
-  history.pushState(pechuguita, '', url);
+  history.pushState(pechuguita, "", url);
   document.title = pechuguita;
 });
 
 function changeURL() {
-  console.log('holis');
+  console.log("holis");
 }
 
-
-
 // TOOLTIP ON IMGS. le falta laburo a la función todavía
-document.body.addEventListener('contextmenu', showTooltip);
+document.body.addEventListener("contextmenu", showTooltip);
 let tooltipElem;
 let timerTooltip;
 
 function showTooltip(event) {
-  let img = event.target.closest('img');
+  let img = event.target.closest("img");
   if (!img) return;
   event.preventDefault();
   removePreviousTooltip();
@@ -196,36 +194,37 @@ function showTooltip(event) {
   timerTooltip = setTimeout(hideTooltip, 1500);
 
   function removePreviousTooltip() {
-    if(timerTooltip) {
+    if (timerTooltip) {
       clearTimeout(timerTooltip);
-    };
+    }
     while (tooltipElem) {
       tooltipElem.remove();
       tooltipElem = null;
-    };
-  };
-  
+    }
+  }
+
   function createTooltip() {
-    tooltipElem = document.createElement('div');
-    tooltipElem.className = 'tooltip';
-    tooltipElem.innerText = '© 2022 Dafna Szleifer';
+    tooltipElem = document.createElement("div");
+    tooltipElem.className = "tooltip";
+    tooltipElem.innerText = "© 2022 Dafna Szleifer";
     document.body.append(tooltipElem);
     // position it above the annotated element (top-center)
     let coords = img.getBoundingClientRect();
     let left = coords.left + (img.offsetWidth - tooltipElem.offsetWidth) / 2;
     if (left < 0) left = 0; // don't cross the left window edge
     let top = coords.top - tooltipElem.offsetHeight - 5;
-    if (top < 0) { // if crossing the top window edge, show below instead
+    if (top < 0) {
+      // if crossing the top window edge, show below instead
       top = coords.top + img.offsetHeight + 5;
     }
-    tooltipElem.style.left = left + 'px';
-    tooltipElem.style.top = top + 'px';
-  };
+    tooltipElem.style.left = left + "px";
+    tooltipElem.style.top = top + "px";
+  }
 
   function hideTooltip() {
     tooltipElem.remove();
     tooltipElem = null;
   }
-};
+}
 
 // una función = una tarea
