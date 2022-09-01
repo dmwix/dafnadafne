5c7201cacdddbf6782dcd5d4bee7768f24133574
dmwix <87998561+dmwix@users.noreply.github.com>
2022-09-01 19:27:09 -0300

    -

---
+++ js/photos.js
@@ -0,0 +1,17 @@
+const photos = [
+    {
+        src:{
+            small: '',
+            medium: '',
+            large: '',
+            webp: ''
+        },
+        tags: [
+            'playa',
+            'perros'
+        ],
+        title: '',
+        slug: '',
+        home_orientation: 'landscape'
+    }
+]
