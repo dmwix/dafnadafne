a5d81c229dc070e80149cfd6358294c5fbc47fa5
dmwix <87998561+dmwix@users.noreply.github.com>
2022-09-04 17:19:20 -0300

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -1,17 +1,79 @@
 const photos = [
-    {
-        src:{
-            small: '',
-            medium: '',
-            large: '',
-            webp: ''
-        },
-        tags: [
-            'playa',
-            'perros'
-        ],
-        title: '',
-        slug: '',
-        home_orientation: 'landscape'
-    }
-]
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "images/0001.jpg",
+      webp: "images/0001.webp",
+    },
+    tags: ["balnearias", "señorxs", "compañia"],
+    title: "señoras de espalda",
+    slug: "senoras-de-espalda",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "images/0002.jpg",
+      webp: "",
+    },
+    tags: ["balnearias", "señorxs"],
+    title: "señor de rojo que juega a las bochas",
+    slug: "senor-rojo-bochas",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "images/0003.jpg",
+      webp: "",
+    },
+    tags: ["señorxs", "fucsia", "compañia"],
+    title: "señoras abrigadas",
+    slug: "senoras-abrigadas",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "images/0004.jpg",
+      webp: "",
+    },
+    tags: ["balnearias", "señorxs", "compañia"],
+    title: "señoras de espalda en mardel",
+    slug: "senoras-de-espalda-en-mardel",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "images/0005.jpg",
+      webp: "",
+    },
+    tags: ["balnearias", "señorxs", "compañia"],
+    title: "señoras con malla de pie",
+    slug: "senoras-con-malla-de-pie",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "images/0006.jpg",
+      webp: "",
+    },
+    tags: ["señorxs", "fucsia"],
+    title: "wells fargo",
+    slug: "wells-fargo",
+    home_orientation: "landscape",
+  },
+];
