1ec8c7220e1bae0a19acee98c568746693b34b79
dmwix <87998561+dmwix@users.noreply.github.com>
2022-09-16 18:17:20 -0300

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -6,7 +6,7 @@ const photos = [
       large: "/images/0001.jpg",
       webp: "/images/0001.webp",
     },
-    tags: ["todas", "balnearias", "señorxs", "compañia"],
+    tags: ["todas", "balnearias", "señorxs", "compañía"],
     title: "señoras de espalda",
     slug: "senoras-de-espalda",
     home_orientation: "portrait",
@@ -32,7 +32,7 @@ const photos = [
       large: "/images/0003.jpg",
       webp: "",
     },
-    tags: ["todas", "señorxs", "fucsia", "compañia"],
+    tags: ["todas", "señorxs", "fucsia", "compañía"],
     title: "señoras abrigadas",
     slug: "senoras-abrigadas",
     home_orientation: "landscape",
@@ -45,9 +45,9 @@ const photos = [
       large: "/images/0004.jpg",
       webp: "",
     },
-    tags: ["todas", "balnearias", "señorxs", "compañia"],
-    title: "señoras de espalda en mardel",
-    slug: "senoras-de-espalda-en-mardel",
+    tags: ["todas", "señorxs", "fucsia"],
+    title: "señora fucsia agachada",
+    slug: "senora-fucsia-agachada",
     home_orientation: "landscape",
   },
 
@@ -58,7 +58,72 @@ const photos = [
       large: "/images/0005.jpg",
       webp: "",
     },
-    tags: ["todas", "balnearias", "señorxs", "compañia"],
+    tags: ["todas", "autos", "balnearias"],
+    title: "auto verde",
+    slug: "auto-verde",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0006.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs"],
+    title: "algodones en los ojos",
+    slug: "algodones-en-los-ojos",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0007.jpg",
+      webp: "",
+    },
+    tags: ["todas", "balnearias", "señorxs", "compañía"],
+    title: "señoras de espalda en mardel",
+    slug: "senoras-de-espalda-en-mardel",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0008.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "balnearias"],
+    title: "fresh thinking",
+    slug: "fresh-thinking",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0009.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "balnearias"],
+    title: "brazos en jarra",
+    slug: "brazos-en-jarra",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0010.jpg",
+      webp: "",
+    },
+    tags: ["todas", "balnearias", "señorxs", "compañía"],
     title: "señoras con malla de pie",
     slug: "senoras-con-malla-de-pie",
     home_orientation: "portrait",
@@ -68,7 +133,7 @@ const photos = [
     src: {
       small: "",
       medium: "",
-      large: "/images/0006.jpg",
+      large: "/images/0011.jpg",
       webp: "",
     },
     tags: ["todas", "señorxs", "fucsia"],
@@ -81,83 +146,181 @@ const photos = [
     src: {
       small: "",
       medium: "",
-      large: "/images/1.png",
-      webp: "",
-    },
-    tags: ["todas", "compañia", "balnearias"],
-    title: "surfers abrazadxs",
-    slug: "surfers-abrazadxs",
-  },
-
-  {
-    src: {
-      small: "",
-      medium: "",
-      large: "/images/2.png",
-      webp: "",
-    },
-    tags: ["todas", "balnearias"],
-    title: "panchos rocco",
-    slug: "panchos-rocco",
-  },
-
-  {
-    src: {
-      small: "",
-      medium: "",
-      large: "/images/4.png",
+      large: "/images/0012.jpg",
       webp: "",
     },
     tags: ["todas", "señorxs", "balnearias"],
-    title: "pelirroja en escalera",
-    slug: "pelirroja-escalera",
+    title: "oriental al telefono",
+    slug: "oriental-al-telefono",
+    home_orientation: "landscape",
   },
 
   {
     src: {
       small: "",
       medium: "",
-      large: "/images/5.png",
+      large: "/images/0013.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "rincones"],
+    title: "peluqueria",
+    slug: "peluqueria",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0014.jpg",
+      webp: "",
+    },
+    tags: ["todas", "balnearias"],
+    title: "pirulines",
+    slug: "pirulines",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0015.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "balnearias"],
+    title: "b1c",
+    slug: "b1c",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0016.jpg",
+      webp: "",
+    },
+    tags: ["todas", "autos"],
+    title: "hot wheels",
+    slug: "hot-wheels",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0017.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "balnearias"],
+    title: "pelirroja al sol",
+    slug: "pelirroja-al-sol",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0018.jpg",
+      webp: "",
+    },
+    tags: ["todas"],
+    title: "sombrilla azul",
+    slug: "sombrilla-azul",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0019.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "fucsia", "balnearias"],
+    title: "chomba rosa",
+    slug: "chomba-rosa",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0020.jpg",
+      webp: "",
+    },
+    tags: ["todas"],
+    title: "pileta",
+    slug: "pileta",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0021.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "balnearias"],
+    title: "sombrillas de colores",
+    slug: "sombrillas-de-colores",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0022.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "balnearias"],
+    title: "ciudad feliz",
+    slug: "ciudad-feliz",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0023.jpg",
+      webp: "",
+    },
+    tags: ["todas", "perros", "fucsia"],
+    title: "caniches",
+    slug: "caniches",
+    home_orientation: "portrait",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0024.jpg",
+      webp: "",
+    },
+    tags: ["todas", "señorxs", "compañía"],
+    title: "mickey",
+    slug: "mickey",
+    home_orientation: "landscape",
+  },
+
+  {
+    src: {
+      small: "",
+      medium: "",
+      large: "/images/0025.jpg",
       webp: "",
     },
     tags: ["todas", "autos"],
     title: "auto rojo",
     slug: "auto-rojo",
-  },
-
-  {
-    src: {
-      small: "",
-      medium: "",
-      large: "/images/6.png",
-      webp: "",
-    },
-    tags: ["todas", "señorxs", "balnearias"],
-    title: "tomando sol en las piedras",
-    slug: "sol-en-piedras",
-  },
-
-  {
-    src: {
-      small: "",
-      medium: "",
-      large: "/images/7.png",
-      webp: "",
-    },
-    tags: ["todas", "señorxs"],
-    title: "carolina",
-    slug: "carolina",
-  },
-
-  {
-    src: {
-      small: "",
-      medium: "",
-      large: "/images/10.png",
-      webp: "",
-    },
-    tags: ["todas", "rincones"],
-    title: "sombrilla azul",
-    slug: "sombrilla-azul",
+    home_orientation: "portrait",
   },
 ];
