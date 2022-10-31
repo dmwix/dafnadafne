a3adafb3d20016184f922e451e883fe44f373844
dmwix <87998561+dmwix@users.noreply.github.com>
2022-10-31 12:21:09 -0300

    Update route.js

--- js/route.js
+++ js/route.js
@@ -1,8 +1,8 @@
 const base = "";
 
 let url_parts = location.pathname.split("/").filter((p) => p && p != base);
+let slugSection = url_parts[0] || false;
 let slugFiltro = url_parts[1] || false;
-let slugFoto = url_parts[2] || false;
 
 // document.querySelector("#data").innerHTML = `
 //         filtro: ${filtro}<br>
