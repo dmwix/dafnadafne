998c49bb16e67509a165a60519a8f88d09211608
dmwix <87998561+dmwix@users.noreply.github.com>
2022-11-17 23:18:52 -0300

    Update route.js

--- js/route.js
+++ js/route.js
@@ -3,8 +3,3 @@ const base = "";
 let url_parts = location.pathname.split("/").filter((p) => p && p != base);
 let slugSection = url_parts[0] || false;
 let slugFiltro = url_parts[1] || false;
-
-// document.querySelector("#data").innerHTML = `
-//         filtro: ${filtro}<br>
-//         foto: ${foto}
-//     `;
